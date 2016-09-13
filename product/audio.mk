1862a7320c167fb747d57605c56d15de2bfd8807
RonGokhale <cips173@gmail.com>
2016-09-13 19:32:26 +0000

    nicki: Switch to new alsa audio hal
     - mixer_paths replace ucm file
     - Updated audio policy for new hal
     - Added custom routes and effects file
     - Fluence props updated for new hal
     - Removed audio defines useless for new hal
    
    Change-Id: I21c73ffd50e81f2dae46f89d738b2112cfc4c4b3

--- product/audio.mk
+++ product/audio.mk
@@ -1,15 +1,18 @@
 # Audio configurations
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/snd_soc_msm_Sitar:system/etc/snd_soc_msm/snd_soc_msm_Sitar
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
 # Audio properties
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.audio.dualmic.config=endfire \
+    persist.audio.fluence.voicecall=true \
+    persist.audio.fluence.voicecomm=true \
+    persist.audio.fluence.voicerec=false \
+    persist.audio.fluence.speaker=true \
+    use.dedicated.device.for.voip=true \
     media.aac_51_output_enabled=true \
-    persist.audio.fluence.mode=endfire \
-    persist.audio.vr.enable=false \
-    persist.audio.handset.mic=digital \
-    persist.audio.lowlatency.rec=false \
-    qcom.audio.init=complete \
-    ro.qc.sdk.audio.ssr=false \
+    audio.offload.disable=0 \
     ro.qc.sdk.audio.fluencetype=fluence
--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -1,6 +1,8 @@
 # Audio permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml
+    frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml
 
 # Audio packages
 PRODUCT_PACKAGES += \
@@ -8,7 +10,8 @@ PRODUCT_PACKAGES += \
     audio.primary.msm8960 \
     audio.r_submix.default \
     audio.usb.default \
-    libaudio-resampler
+    libaudio-resampler \
+    libqcomvoiceprocessing
 
 # Audio tools
 PRODUCT_PACKAGES += \
