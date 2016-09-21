ac2c55de2444af7a809fe8bb4166c8af12a0e376
Mahesh <mahesh140494@gmail.com>
2016-09-21 07:03:02 +0000

    nicki: multirom: Update to NO_KEXEC version 4
    Change-Id: Id18f95bb9c61a11efec7d0d50d8643f9ba1f2dae

--- board/multirom.mk
+++ board/multirom.mk
@@ -2,11 +2,11 @@
 MR_INPUT_TYPE := type_b
 MR_INIT_DEVICES := device/sony/nicki/multirom/mr_init_devices.c
 MR_DPI := hdpi
-MR_DPI_FONT := 160
+MR_DPI_FONT := 180
 MR_FSTAB := device/sony/nicki/rootdir/twrp.fstab
 MR_USE_MROM_FSTAB := true
 MR_KEXEC_MEM_MIN := 0x85000000
-MR_DEVICE_VARIANTS := C1904 C1905 C2004 C2005 nicki
+MR_DEVICE_VARIANTS := C1904 C1905 C2004 C2005
 MR_UNIFIED_TABS := true
 
 # MultiROM build
@@ -14,4 +14,4 @@ DEVICE_RESOLUTION := 720x1280
 TARGET_RECOVERY_IS_MULTIROM := true
 
 # MultiROM no-kexec workaround
-MR_ALLOW_NKK71_NOKEXEC_WORKAROUND := true
+MR_NO_KEXEC := enabled
