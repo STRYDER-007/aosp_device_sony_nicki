b28ff5fc369173c91f42c8df64eebd4f7b85efb1
Mahesh Mahimane <mahesh140494@gmail.com>
2017-04-29 10:09:07 +0000

    nicki: build: Add AOSP-CAF 7.1 build support

--- aosp_nicki.mk
+++ aosp_nicki.mk
@@ -15,8 +15,8 @@
 # limitations under the License.
 #
 
-# Inherit common Lineage stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit common AOSP-CAF stuff
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Inherit device configurations
 $(call inherit-product, device/sony/nicki/device.mk)
@@ -27,7 +27,7 @@ TARGET_SCREEN_WIDTH := 480
 
 # Device identifications
 PRODUCT_DEVICE := nicki
-PRODUCT_NAME := lineage_nicki
+PRODUCT_NAME := aosp_nicki
 PRODUCT_BRAND := Sony
 PRODUCT_MANUFACTURER := Sony
 PRODUCT_MODEL := nicki
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,5 +14,5 @@
 # limitations under the License.
 #
 
-add_lunch_combo lineage_nicki-eng
-add_lunch_combo lineage_nicki-userdebug
+add_lunch_combo aosp_nicki-eng
+add_lunch_combo aosp_nicki-userdebug
